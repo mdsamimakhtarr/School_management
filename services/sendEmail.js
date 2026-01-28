c1288ec555553007cf331512b87e19ff44fa17ef
mdsamim akhtar <mdsamimakhtar0000@gmail.com>
2026-01-28 20:53:48 +0530

    intial commit

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, // gmail address
-    pass: process.env.EMAIL_PASS, // GOOGLE APP PASSWORD
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
   },
 });
 
